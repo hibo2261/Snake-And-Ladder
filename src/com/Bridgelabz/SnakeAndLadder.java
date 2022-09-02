3e27d21fa459ef3e39cb006b7749d6fd7da92af0
hibo2261 <hborse1@gmail.com>
2022-09-02 09:59:01 +0530

    [Himanshu]: Added Welcome Message

---
+++ src/com/Bridgelabz/SnakeAndLadder.java
@@ -0,0 +1,14 @@
+package com.Bridgelabz;
+public class SnakeAndLadder {
+
+    public static void main(String[] args)
+    {
+        System.out.println("*********************************");
+        System.out.println("Welcome");
+        System.out.println("To");
+        System.out.println("Snake And Ladder Game");
+        System.out.println("Author = Himanshu Borse");
+
+        System.out.println("*********************************");
+    }
+}
